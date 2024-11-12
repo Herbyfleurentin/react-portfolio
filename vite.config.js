40f75c27f543d604eaedbbccfb8c75f1f1720a54
Herby Fleurentin <fleurentinherby7@yahoo.fr>
2024-11-12 15:13:29 +0100

    Préparation du déploiement

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/react-portfolio/', // Remplacez par le nom de votre dépôt
   plugins: [react()],
   css:{
     modules:{
@@ -10,3 +11,4 @@ export default defineConfig({
     }
   }
 })
+
